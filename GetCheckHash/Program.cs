0bd04e9659e86247480f42408ed4f7f45114acee
Bogdan <40561032+BogdanPinchuk@users.noreply.github.com>
2021-06-16 11:22:09 +0300

    Added checking and creating files

--- GetCheckHash/Program.cs
+++ GetCheckHash/Program.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace GetCheckHash
 {
     class Program
     {
+        /// <summary>
+        /// Локалізація інтерфейсу
+        /// </summary>
+        enum Localization
+        {
+            /// <summary>
+            /// Англійська
+            /// </summary>
+            English,
+            /// <summary>
+            /// Українська
+            /// </summary>
+            Ukrainian,
+        }
+
+        /// <summary>
+        /// Алгоритм розрахунку хеш-кодів
+        /// </summary>
         enum HashNames
         {
             /// <summary>
@@ -53,24 +70,141 @@ namespace GetCheckHash
             // відображеня кирилиці
             Console.OutputEncoding = Encoding.Unicode;
 
-            //// синхронізація доступу
-            //object block = new object();
-
-            // директорія
-            string path = Directory.GetCurrentDirectory();
-
-            // список файлів в каталогах і підкаталогах
-            string[] fileNames = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
-
             // створення словника хеш-кодів всіх файлів в папці
-            Dictionary<string, string> hashCodes = new Dictionary<string, string>();
+            Dictionary<string, string> hashCodes = new();
 
             // алгоритм розрахунку
             HashNames algorithm = HashNames.MD5;
 
-            // обчислення хеш-кодів
-            for (int i = 0; i < fileNames.Length; i++)
-                hashCodes.Add(fileNames[i], GetHashCode(fileNames[i], algorithm));
+            while (true)
+            {
+                // запит на вибір
+                Console.WriteLine("What do you want?\n1. Checking files with md5 hash files?\n2. Create md5 hash file for each file?\n0. Exit.\n");
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                // Перевірка файлів
+                if (key == ConsoleKey.D1 || key == ConsoleKey.NumPad1)
+                {
+                    // директорія
+                    string path = Directory.GetCurrentDirectory();
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Current directory:\n{path}\n");
+                    Console.ResetColor();
+
+                    // список файлів і хеш-файлів в каталогах і підкаталогах
+                    List<string> listFileNames = new();
+                    List<string> listHashNames = new();
+                    listFileNames.AddRange(Directory.GetFiles(path, "*", SearchOption.AllDirectories));
+                    // копіювання
+                    listHashNames.AddRange(listFileNames);
+
+                    listFileNames.RemoveAll(i => new FileInfo(i).Extension.ToLower() ==
+                        $".{Enum.GetName(algorithm).ToString().ToLower()}");
+                    listHashNames.RemoveAll(i => new FileInfo(i).Extension.ToLower() !=
+                        $".{Enum.GetName(algorithm).ToString().ToLower()}");
+
+                    string[] fileNames = listFileNames.ToArray(),
+                        hashNames = listHashNames.ToArray();
+
+                    listFileNames.Clear();
+                    //listHashNames.Clear();
+
+                    // сповіщення про відсутність файлів
+                    if (fileNames.Length != hashNames.Length)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Not all files have a hash file!");
+                        Console.ResetColor();
+                    }
+
+                    // розрахунок хеш-кодів
+                    hashCodes.Clear();
+                    for (int i = 0; i < fileNames.Length; i++)
+                        hashCodes.Add(fileNames[i], GetHashCode(fileNames[i], algorithm));
+
+                    // перевірка хеш-файлів
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Start checking hash-files:");
+                    Console.ResetColor();
+
+                    foreach (var hashName in listHashNames)
+                    {
+                        try
+                        {
+                            string fileName = hashName.Replace($".{Enum.GetName(algorithm).ToString().ToLower()}", "");
+                            CheckData(hashName, hashCodes[fileName], algorithm);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(ex.Message);
+                            Console.ResetColor();
+                        }
+                    }
+
+                    // завершення роботи
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("All hash files were checked!\n");
+                    Console.ResetColor();
+                    // чекати до виходу
+                    Console.ReadKey(true);
+                    break;
+                }
+                // Створення файлів
+                else if (key == ConsoleKey.D2 || key == ConsoleKey.NumPad2)
+                {
+                    // директорія
+                    string path = Directory.GetCurrentDirectory();
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Current directory:\n{path}\n");
+                    Console.ResetColor();
+
+                    // список файлів в каталогах і підкаталогах
+                    List<string> listFileNames = new();
+                    listFileNames.AddRange(Directory.GetFiles(path, "*", SearchOption.AllDirectories));
+                    listFileNames.RemoveAll(i => new FileInfo(i).Extension.ToLower() ==
+                        $".{Enum.GetName(algorithm).ToString().ToLower()}"); ;
+                    string[] fileNames = listFileNames.ToArray();
+                    listFileNames.Clear();
+
+                    // розрахунок хеш-кодів
+                    hashCodes.Clear();
+                    for (int i = 0; i < fileNames.Length; i++)
+                        hashCodes.Add(fileNames[i], GetHashCode(fileNames[i], algorithm));
+
+                    // створення хеш-файлів
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Start creating hash-files:");
+                    Console.ResetColor();
+                    foreach (var hash in hashCodes)
+                        SaveData(hash.Key, hash.Value, algorithm);
+
+                    // завершення роботи
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("All hash files were created succesful!\n");
+                    Console.ResetColor();
+
+                    // чекати до виходу
+                    Console.ReadKey(true);
+                    break;
+                }
+                // вихід
+                else if (key == ConsoleKey.D0 || key == ConsoleKey.NumPad0)
+                {
+                    break;
+                }
+                // повторний запит
+                else
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+            }
+            #region MultiThreading
+            //// синхронізація доступу
+            //object block = new object();
+
             // багатопотоковість
             //Parallel.For(0, fileNames.Length,
             //    i =>
@@ -80,34 +214,7 @@ namespace GetCheckHash
             //            hashCodes.Add(fileNames[i], GetHashCode(fileNames[i], algorithm));
             //        }
             //    });
-
-            // збереження даних
-            Console.WriteLine("Do you want save data (Y/N)? Data will be rewriter!");
-            ConsoleKey key = Console.ReadKey().Key;
-            if (key == ConsoleKey.Y || key == ConsoleKey.Enter)
-            {
-
-            }
-
-
-
-
-            foreach (var item in hashCodes)
-            {
-                //Console.WriteLine(item.ToString());
-                Console.WriteLine(item.Key.Replace(path + @"\", string.Empty));
-            }
-
-            Console.WriteLine("\nМоя папка:");
-            Console.WriteLine(path);
-            // результати
-            //string result = default;
-
-
-
-            //Console.WriteLine(result);
-
-            Console.ReadKey(true);
+            #endregion
         }
 
         /// <summary>
@@ -149,31 +256,73 @@ namespace GetCheckHash
         /// <summary>
         /// Збереження даних хеш-кодів
         /// </summary>
-        /// <param name="path">шлях до файлу</param>
+        /// <param name="fileName">шлях до файлу</param>
+        /// <param name="hashcode">хеш-код</param>
         /// <param name="hashName">метод</param>
-        /// <param name="hashCodes">хеш-коди</param>
-        private static void SaveData(string path, HashNames hashName, Dictionary<string, string> hashCodes)
+        private static void SaveData(string fileName, string hashcode, HashNames hashName)
         {
-            string fileName = path +
-                @"\HathCodes." +
-                Enum.GetName(hashName).ToString().ToLower();
+            // назва нового файлу
+            string newName = fileName + ".md5";
 
             try
             {
-                using (StreamWriter sw = new StreamWriter(fileName, false, Encoding.UTF8))
+                using (StreamWriter sw = new(newName, false, Encoding.UTF8))
                 {
-                    var data = new StringBuilder();
-                    foreach (var hash in hashCodes)
-                        data.AppendLine($"{hash.Value} *{new FileInfo(hash.Key).Name}");
+                    string data = hashcode + " *" + new FileInfo(fileName).Name;
+                    sw.Write(data);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(data);
+                    Console.ResetColor();
                 }
             }
             catch (Exception ex)
             {
-                var color = Console.BackgroundColor;
-                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(ex.Message);
-                Console.BackgroundColor = color;
+                Console.ResetColor();
             }
         }
+
+        /// <summary>
+        /// Перевірка хешів файлів
+        /// </summary>
+        /// <param name="fileName">шлях до хеш-файлу</param>
+        /// <param name="hashcode">хеш-код</param>
+        /// <param name="hashName">метод</param>
+        private static void CheckData(string fileName, string hashcode, HashNames hashName)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName, Encoding.UTF8))
+                {
+                    string data = sr.ReadToEnd();
+                    string header = string.Empty;
+
+                    // перевірка хеш-коду
+                    if (data.Contains(hashcode))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        header = "correct file: ";
+                        Console.WriteLine(header + data);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        header = "incorrect file: ";
+                        Console.WriteLine(header + data);
+                        Console.WriteLine("hash code of file: " + hashcode);
+                    }
+
+                    Console.ResetColor();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+        }
+
     }
 }
